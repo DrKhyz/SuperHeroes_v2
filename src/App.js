3bbc11582dcc964a2b230b1dbf9e5f5e9350c17a
Jonathan Galvao Diniz <jo.diniz@live.fr>
2019-04-10 14:48:26 +0200

    adding component with stars

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import CardLife from './component/CardLife/CardLife';
 import CardStats from './component/CardStats/CardStats';
+import CardStars from './component/CardStars/CardStars';
 
 class App extends Component {
 	render() {
 		return (
 			<div>
+				<CardStars />
 				<CardStats />
 			</div>
 		);
---
+++ src/component/CardStars/CardStars.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+const CardStars = () => {
+	return (
+		<div>
+			<img src='https://img.icons8.com/color/48/000000/filled-star.png' />
+			<img src='https://img.icons8.com/color/48/000000/filled-star.png' />
+			<img src='https://img.icons8.com/color/48/000000/filled-star.png' />
+			<img src='https://img.icons8.com/color/48/000000/star.png' />
+			<img src='https://img.icons8.com/color/48/000000/star.png' />
+		</div>
+	);
+};
+
+export default CardStars;
