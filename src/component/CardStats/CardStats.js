db9952c91a0f30c1730936f028fc5d3ca301bd02
Stéphane Vivier <contact.xsprod@gmail.com>
2019-04-10 10:26:02 +0200

    Add a table for stats

---
+++ src/component/CardStats/CardStats.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Table } from 'reactstrap';
+import './CardStats.css';
+
+class CardStats extends React.Component {
+	render() {
+		return (
+			<div className='card_stats'>
+				<Table>
+					<thead>
+						<tr>
+							<th>INT</th>
+							<th>STR</th>
+							<th>SPD</th>
+							<th>END</th>
+							<th>POW</th>
+							<th>CBT</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>100</td>
+							<td>100</td>
+							<td>100</td>
+							<td>100</td>
+							<td>100</td>
+							<td>100</td>
+						</tr>
+					</tbody>
+				</Table>
+			</div>
+		);
+	}
+}
+
+export default CardStats;
