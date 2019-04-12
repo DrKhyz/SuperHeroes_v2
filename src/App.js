243a0c8095a30deaef5496a4f8e20ef3d09bed88
Baptiste Hyver <baptiste.hyver@gmail.com>
2019-04-12 09:17:32 +0200

    implemented the loader while waiting for an answer from the API

--- src/App.js
+++ src/App.js
@@ -6,23 +6,31 @@ import CardLife from './components/CardLife/CardLife';
 import ButtonRandomHero from './components/Buttons/ButtonRandomHero';
 import ButtonFight from './components/Buttons/ButtonFight';
 import './App.css';
+import { Spinner } from 'reactstrap';
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			picture: ''
+			isLoading: true
 		};
 	}
 
+	componentDidMount() {
+		this.getCaracter1();
+		this.getCaracter2();
+	}
+
 	getCaracter1() {
-		const getRandomInt = max => {
+		this.setState({ isLoading: true });
+		const getRandomInt1 = max => {
 			return Math.floor(Math.random() * Math.floor(max));
 		};
-		fetch(`https://www.superheroapi.com/api.php/1241580159334538/${getRandomInt(729)}`)
+		fetch(`https://superheroapi.com/api.php/2427014800851400/${getRandomInt1(720)}`)
 			.then(response => response.json())
 			.then(data => {
 				this.setState({
+					isLoading: false,
 					picture: data.image.url
 				});
 			});
@@ -32,14 +40,42 @@ class App extends Component {
 		const getRandomInt2 = max => {
 			return Math.floor(Math.random() * Math.floor(max));
 		};
-		fetch(`https://superheroapi.com/api.php/2427014800851400/${getRandomInt2(729)}`)
+		fetch(`https://superheroapi.com/api.php/2427014800851400/${getRandomInt2(720)}`)
 			.then(response => response.json())
 			.then(data => {
-				this.setState({ picture2: data.image.url });
+				this.setState({
+					isLoading: false,
+					picture2: data.image.url
+				});
 			});
 	}
 
 	render() {
+		if (this.state.isLoading) {
+			return (
+				<div className='cardDisplay'>
+					<p>
+						It's loading... <br />
+						<Spinner style={{ width: '5rem', height: '5rem' }} color='primary' />
+					</p>
+					<div className='buttonDisplay'>
+						<ButtonFight />
+						<ButtonRandomHero
+							selectHero={() => {
+								this.getCaracter1();
+								this.getCaracter2();
+							}}
+						/>
+						<CardLife />
+						<CardStars />
+					</div>
+					<p>
+						It's loading... <br />
+						<Spinner style={{ width: '5rem', height: '5rem' }} color='primary' />
+					</p>
+				</div>
+			);
+		}
 		return (
 			<div className='cardDisplay'>
 				<Card imgUrl={this.state.picture} />
