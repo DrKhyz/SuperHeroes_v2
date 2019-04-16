e6287b8a498a3fa06aa1f4cc4edbecebed874cda
Baptiste Hyver <baptiste.hyver@gmail.com>
2019-04-16 09:26:38 +0200

    Realized the css to put in place the components on the card

--- src/components/Buttons/ButtonFight.jsx
+++ src/components/Buttons/ButtonFight.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Button } from 'reactstrap';
+import './Button.css';
 
-const ButtonFight = ({ selectOpponent }) => {
-    return (
-        <div>
-            <Button color="danger">Fight !</Button>
-        </div>
-    );
+const ButtonFight = ({ Fight }) => {
+	return (
+		<div className='Button_fight'>
+			<Button color='danger'>Fight !</Button>
+		</div>
+	);
 };
 
 export default ButtonFight;
