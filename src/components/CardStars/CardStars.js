24d75c8454164ae22c11996209be5afa2bf2bc13
Baptiste Hyver <baptiste.hyver@gmail.com>
2019-04-23 20:19:07 +0200

    Added a conditionnal rendering

--- src/components/CardStars/CardStars.js
+++ src/components/CardStars/CardStars.js
@@ -1,7 +1,60 @@
 import React from 'react';
 import './CardStars.css';
 
-const CardStars = () => {
+
+const CardStars = (props) => {
+	let int = parseInt(props.powerstats.intelligence);
+	let str = parseInt(props.powerstats.strength);
+	let spd = parseInt(props.powerstats.speed);
+	let end = parseInt(props.powerstats.durability);
+	let pow = parseInt(props.powerstats.power);
+	let cbt = parseInt(props.powerstats.combat);
+	let average = (int + str + spd + end + pow + cbt) / 6;
+	if (average >= 0 && average <= 19) {
+		return <div>
+			<img src='https://img.icons8.com/color/32/000000/filled-star.png' alt='filled star' />
+			<img src='https://img.icons8.com/color/32/000000/star.png' alt='star' />
+			<img src='https://img.icons8.com/color/32/000000/star.png' alt='star' />
+			<img src='https://img.icons8.com/color/32/000000/star.png' alt='star' />
+			<img src='https://img.icons8.com/color/32/000000/star.png' alt='star' />
+		</div>
+	}
+	if (average >= 20 && average <= 39) {
+		return <div>
+			<img src='https://img.icons8.com/color/32/000000/filled-star.png' alt='filled star' />
+			<img src='https://img.icons8.com/color/32/000000/filled-star.png' alt='filled star' />
+			<img src='https://img.icons8.com/color/32/000000/star.png' alt='star' />
+			<img src='https://img.icons8.com/color/32/000000/star.png' alt='star' />
+			<img src='https://img.icons8.com/color/32/000000/star.png' alt='star' />
+		</div>
+	}
+	if (average >= 40 && average <= 59) {
+		return <div>
+			<img src='https://img.icons8.com/color/32/000000/filled-star.png' alt='filled star' />
+			<img src='https://img.icons8.com/color/32/000000/filled-star.png' alt='filled star' />
+			<img src='https://img.icons8.com/color/32/000000/filled-star.png' alt='filled star' />
+			<img src='https://img.icons8.com/color/32/000000/star.png' alt='star' />
+			<img src='https://img.icons8.com/color/32/000000/star.png' alt='star' />
+		</div>
+	}
+	if (average >= 60 && average <= 79) {
+		return <div>
+			<img src='https://img.icons8.com/color/32/000000/filled-star.png' alt='filled star' />
+			<img src='https://img.icons8.com/color/32/000000/filled-star.png' alt='filled star' />
+			<img src='https://img.icons8.com/color/32/000000/filled-star.png' alt='filled star' />
+			<img src='https://img.icons8.com/color/32/000000/filled-star.png' alt='filled star' />
+			<img src='https://img.icons8.com/color/32/000000/star.png' alt='star' />
+		</div>
+	}
+	if (average >= 80 && average <= 100) {
+		return <div>
+			<img src='https://img.icons8.com/color/32/000000/filled-star.png' alt='filled star' />
+			<img src='https://img.icons8.com/color/32/000000/filled-star.png' alt='filled star' />
+			<img src='https://img.icons8.com/color/32/000000/filled-star.png' alt='filled star' />
+			<img src='https://img.icons8.com/color/32/000000/filled-star.png' alt='filled star' />
+			<img src='https://img.icons8.com/color/32/000000/filled-star.png' alt='filled star' />
+		</div>
+	}
 	return (
 		<div className='stars'>
 			<img src='https://img.icons8.com/color/32/000000/filled-star.png' alt='filled star' />
@@ -10,7 +63,7 @@ const CardStars = () => {
 			<img src='https://img.icons8.com/color/32/000000/star.png' alt='star' />
 			<img src='https://img.icons8.com/color/32/000000/star.png' alt='star' />
 		</div>
-	);
+	)
 };
 
 export default CardStars;
