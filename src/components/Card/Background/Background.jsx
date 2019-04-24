fb172311752b3a5cce43a0248eaca7d2348e8550
Jonathan Galvao Diniz <jo.diniz@live.fr>
2019-04-24 09:28:46 +0200

    fixing missing background

--- src/components/Card/Background/Background.jsx
+++ src/components/Card/Background/Background.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
-import './Background.css';
+import React from 'react'
+import './Background.css'
 
 const Background = props => {
-	return <img className='background_img' src={props.image.url} alt='' />;
-};
+	return (
+		<img
+			onError={e => {
+				e.target.onerror = null
+				e.target.src =
+					'https://image.shutterstock.com/image-vector/404-error-page-not-found-450w-603779531.jpg'
+			}}
+			className='background_img'
+			src={props.image.url}
+			alt=''
+		/>
+	)
+}
 
-export default Background;
+export default Background
