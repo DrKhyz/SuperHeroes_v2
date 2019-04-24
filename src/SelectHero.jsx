101761a248187c33bb180155457201d4f1c9aa9b
Stéphane Vivier <contact.xsprod@gmail.com>
2019-04-24 11:40:34 +0200

    Added a search bar for heros with a standalone component

--- src/SelectHero.jsx
+++ src/SelectHero.jsx
@@ -1,7 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Card from './components/Card/Card.jsx';
 
-export default class selectHero extends Component {
+class SelectHero extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			search: '',
+			heroStore: [],
+			loadingHeroStore: true
+		};
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange = e => {
+		this.setState({ search: e.target.value });
+	};
+
+	handleSubmit = e => {
+		this.setState({ loadingHeroStore: true });
+		fetch(`https://superheroapi.com/api.php/2427014800851400/search/${this.state.search}`)
+			.then(response => response.json())
+			.then(data => this.setState({ heroStore: data.results }))
+			.then(this.setState({ loadingHeroStore: false }));
+		e.preventDefault();
+	};
+
 	render() {
-		return <div>Hello World</div>
+		return (
+			<div>
+				<form onSubmit={this.handleSubmit}>
+					<input type='text' onChange={this.handleChange} value={this.state.search} name='search' id='search' />
+					<button type='submit'>Submit</button>
+				</form>
+				{this.state.loadingHeroStore ? '' : this.state.heroStore.map(heroProps => <Card {...heroProps} />)}
+			</div>
+		);
 	}
 }
+
+export default SelectHero;
