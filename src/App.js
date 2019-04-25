f755570480175bfd7fc7b1e6f6e600f928a66835
Baptiste Hyver <baptiste.hyver@gmail.com>
2019-04-25 15:47:20 +0200

    added a button to get back to landing page in both arena and hero selection

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react'
-import Card from './components/Card/Card'
-import ButtonRandomHero from './components/Buttons/ButtonRandomHero'
-import AlgoCombat from './AlgoCombat'
-import './App.css'
-import { Spinner } from 'reactstrap'
-import ImgVS from './components/ImgVS/ImgVS'
+import React, { Component } from 'react';
+import Card from './components/Card/Card';
+import ButtonRandomHero from './components/Buttons/ButtonRandomHero';
+import AlgoCombat from './AlgoCombat';
+import './App.css';
+import { Spinner, Button } from 'reactstrap';
+// import ImgVS from './components/ImgVS/ImgVS';
+import { NavLink } from 'react-router-dom';
 
 class App extends Component {
 	constructor(props) {
-		super(props)
+		super(props);
 		this.state = {
 			isLoading: true,
 			winner: '',
@@ -23,20 +24,20 @@ class App extends Component {
 					power: '',
 					combat: '',
 					life: '',
-					barColor: '',
+					barColor: ''
 				},
 				biography: {
 					'full-name': '',
 					publisher: '',
-					alignment: '',
+					alignment: ''
 				},
 				appearance: {
 					gender: '',
 					race: '',
 					height: '',
-					weight: '',
+					weight: ''
 				},
-				image: '',
+				image: ''
 			},
 			hero2: {
 				id: '',
@@ -49,51 +50,61 @@ class App extends Component {
 					power: '',
 					combat: '',
 					life: '',
-					barColor: '',
+					barColor: ''
 				},
 				biography: {
 					'full-name': '',
 					publisher: '',
-					alignment: '',
+					alignment: ''
 				},
 				appearance: {
 					gender: '',
 					race: '',
 					height: '',
-					weight: '',
+					weight: ''
 				},
-				image: '',
-			},
-		}
+				image: ''
+			}
+		};
 	}
 
 	ClickCombat() {
-		let test = AlgoCombat(this.state)
-		this.setState(test)
+		let test = AlgoCombat(this.state);
+		this.setState(test);
 	}
 
 	componentDidMount() {
-		this.getCaracter1()
-		this.getCaracter2()
+		this.getCaracter1();
+		this.getCaracter2();
 	}
 
 	getCaracter1() {
-		this.setState({ isLoading: true })
+		this.setState({ isLoading: true });
 		const getRandomInt1 = max => {
-			return Math.floor(Math.random() * Math.floor(max))
-		}
+			return Math.floor(Math.random() * Math.floor(max));
+		};
 		fetch(`https://superheroapi.com/api.php/2427014800851400/${getRandomInt1(720)}`)
 			.then(response => response.json())
 			.then(data => {
 				if (data.powerstats.intelligence === 'null') {
-					data.powerstats.intelligence = Math.floor(Math.random() * Math.floor(101))
-					console.log('Stats généré aléatoirement')
+					data.powerstats.intelligence = Math.floor(Math.random() * Math.floor(101));
+					console.log('Stats généré aléatoirement');
 				}
-				if (data.powerstats.strength === 'null') { data.powerstats.strength = Math.floor(Math.random() * Math.floor(101)) }
-				if (data.powerstats.speed === 'null') { data.powerstats.speed = Math.floor(Math.random() * Math.floor(101)) }
-				if (data.powerstats.durability === 'null') { data.powerstats.durability = Math.floor(Math.random() * Math.floor(101)) }
-				if (data.powerstats.power === 'null') { data.powerstats.power = Math.floor(Math.random() * Math.floor(101)) }
-				if (data.powerstats.combat === 'null') { data.powerstats.combat = Math.floor(Math.random() * Math.floor(101)) }
+				if (data.powerstats.strength === 'null') {
+					data.powerstats.strength = Math.floor(Math.random() * Math.floor(101));
+				}
+				if (data.powerstats.speed === 'null') {
+					data.powerstats.speed = Math.floor(Math.random() * Math.floor(101));
+				}
+				if (data.powerstats.durability === 'null') {
+					data.powerstats.durability = Math.floor(Math.random() * Math.floor(101));
+				}
+				if (data.powerstats.power === 'null') {
+					data.powerstats.power = Math.floor(Math.random() * Math.floor(101));
+				}
+				if (data.powerstats.combat === 'null') {
+					data.powerstats.combat = Math.floor(Math.random() * Math.floor(101));
+				}
 				this.setState({
 					isLoading: false,
 					winner: '',
@@ -109,43 +120,53 @@ class App extends Component {
 							combat: parseInt(data.powerstats.combat),
 							life: Math.floor(
 								(parseInt(data.powerstats.durability) + parseInt(data.powerstats.intelligence)) *
-								(data.powerstats.speed / 10),
+									(data.powerstats.speed / 10)
 							),
-							barColor: 'success',
+							barColor: 'success'
 						},
 						biography: {
 							'full-name': data.biography['full-name'],
 							publisher: data.biography.publisher,
-							alignment: data.biography.alignment,
+							alignment: data.biography.alignment
 						},
 						appearance: {
 							gender: data.appearance.gender,
 							race: data.appearance.race,
 							height: data.appearance.height[1],
-							weight: data.appearance.weight[1],
+							weight: data.appearance.weight[1]
 						},
-						image: data.image,
-					},
-				})
-
-
-			})
+						image: data.image
+					}
+				});
+			});
 	}
 
 	getCaracter2() {
-		this.setState({ isLoading: true })
+		this.setState({ isLoading: true });
 		const getRandomInt2 = max => {
-			return Math.floor(Math.random() * Math.floor(max))
-		}
+			return Math.floor(Math.random() * Math.floor(max));
+		};
 		fetch(`https://superheroapi.com/api.php/2427014800851400/${getRandomInt2(720)}`)
 			.then(response => response.json())
 			.then(data => {
-				if (data.powerstats.intelligence === 'null') { data.powerstats.intelligence = Math.floor(Math.random() * Math.floor(101)) }
-				if (data.powerstats.strength === 'null') { data.powerstats.strength = Math.floor(Math.random() * Math.floor(101)) }
-				if (data.powerstats.speed === 'null') { data.powerstats.speed = Math.floor(Math.random() * Math.floor(101)) }
-				if (data.powerstats.durability === 'null') { data.powerstats.durability = Math.floor(Math.random() * Math.floor(101)) }
-				if (data.powerstats.power === 'null') { data.powerstats.power = Math.floor(Math.random() * Math.floor(101)) }
-				if (data.powerstats.combat === 'null') { data.powerstats.combat = Math.floor(Math.random() * Math.floor(101)) }
+				if (data.powerstats.intelligence === 'null') {
+					data.powerstats.intelligence = Math.floor(Math.random() * Math.floor(101));
+				}
+				if (data.powerstats.strength === 'null') {
+					data.powerstats.strength = Math.floor(Math.random() * Math.floor(101));
+				}
+				if (data.powerstats.speed === 'null') {
+					data.powerstats.speed = Math.floor(Math.random() * Math.floor(101));
+				}
+				if (data.powerstats.durability === 'null') {
+					data.powerstats.durability = Math.floor(Math.random() * Math.floor(101));
+				}
+				if (data.powerstats.power === 'null') {
+					data.powerstats.power = Math.floor(Math.random() * Math.floor(101));
+				}
+				if (data.powerstats.combat === 'null') {
+					data.powerstats.combat = Math.floor(Math.random() * Math.floor(101));
+				}
 				this.setState({
 					isLoading: false,
 					winner: '',
@@ -161,25 +182,25 @@ class App extends Component {
 							combat: parseInt(data.powerstats.combat),
 							life: Math.floor(
 								(parseInt(data.powerstats.durability) + parseInt(data.powerstats.intelligence)) *
-								(data.powerstats.speed / 10),
+									(data.powerstats.speed / 10)
 							),
-							barColor: 'success',
+							barColor: 'success'
 						},
 						biography: {
 							'full-name': data.biography['full-name'],
 							publisher: data.biography.publisher,
-							alignment: data.biography.alignment,
+							alignment: data.biography.alignment
 						},
 						appearance: {
 							gender: data.appearance.gender,
 							race: data.appearance.race,
 							height: data.appearance.height[1],
-							weight: data.appearance.weight[1],
+							weight: data.appearance.weight[1]
 						},
-						image: data.image,
-					},
-				})
-			})
+						image: data.image
+					}
+				});
+			});
 	}
 
 	render() {
@@ -190,49 +211,50 @@ class App extends Component {
 						<Spinner style={{ width: '5rem', height: '5rem' }} color='primary' />
 					</p>
 					<div className='buttonDisplay'>
-						<ImgVS urlImgVS='http://www.sclance.com/pngs/vs-png/vs_png_1474185.png' />
+						{/* <ImgVS urlImgVS='http://www.sclance.com/pngs/vs-png/vs_png_1474185.png' /> */}
 						<ButtonRandomHero
 							selectHero={() => {
-								this.getCaracter1()
-								this.getCaracter2()
+								this.getCaracter1();
+								this.getCaracter2();
 							}}
 						/>
 						<button color='danger'>Fight !</button>
+						<NavLink to='/'>
+							<Button style={{ backgroundColor: '#162CA2', border: '1px solid black' }}>Back to landing page</Button>
+						</NavLink>
 					</div>
 					<p>
 						<Spinner style={{ width: '5rem', height: '5rem' }} color='primary' />
 					</p>
 				</div>
-			)
+			);
 		}
 		return (
 			<div className='cardDisplay'>
 				<Card {...this.state.hero1} />
 				<div className='buttonDisplay'>
-					<ImgVS urlImgVS='http://www.sclance.com/pngs/vs-png/vs_png_1474185.png' />
+					{/* <ImgVS urlImgVS='http://www.sclance.com/pngs/vs-png/vs_png_1474185.png' /> */}
 					<ButtonRandomHero
 						selectHero={() => {
-							this.getCaracter1()
-							this.getCaracter2()
+							this.getCaracter1();
+							this.getCaracter2();
 						}}
 					/>
 					{this.state.hero1.powerstats.life <= 0 || this.state.hero2.powerstats.life <= 0 ? (
-						<div>
-							Le gagnant est :
-							{
-								this.state.winner
-							}
-						</div>
+						<div>Le gagnant est :{this.state.winner}</div>
 					) : (
-							<button onClick={() => this.ClickCombat()} color='danger'>
-								Fight !
+						<button onClick={() => this.ClickCombat()} color='danger'>
+							Fight !
 						</button>
-						)}
+					)}
+					<NavLink to='/'>
+						<Button style={{ backgroundColor: '#162CA2', border: '1px solid black' }}>Back to landing page</Button>
+					</NavLink>
 				</div>
 				<Card {...this.state.hero2} />
 			</div>
-		)
+		);
 	}
 }
 
-export default App
+export default App;
