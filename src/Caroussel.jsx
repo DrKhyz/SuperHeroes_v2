9a088971c85bb857f7127ca88af6a581b1f4609b
Baptiste Hyver <baptiste.hyver@gmail.com>
2019-04-25 14:56:25 +0200

    Took care of the styling and responsivity of the Landing page

---
+++ src/Caroussel.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { Carousel, CarouselItem } from 'reactstrap';
+
+const items = [
+	{
+		src: 'https://comiclists.files.wordpress.com/2010/09/marvel-universe-team-e1284662599805.jpg'
+	}
+];
+
+class Caroussel extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { activeIndex: 0 };
+		this.next = this.next.bind(this);
+		this.previous = this.previous.bind(this);
+		this.goToIndex = this.goToIndex.bind(this);
+		this.onExiting = this.onExiting.bind(this);
+		this.onExited = this.onExited.bind(this);
+	}
+
+	onExiting() {
+		this.animating = true;
+	}
+
+	onExited() {
+		this.animating = false;
+	}
+
+	next() {
+		if (this.animating) return;
+		const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
+		this.setState({ activeIndex: nextIndex });
+	}
+
+	previous() {
+		if (this.animating) return;
+		const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
+		this.setState({ activeIndex: nextIndex });
+	}
+
+	goToIndex(newIndex) {
+		if (this.animating) return;
+		this.setState({ activeIndex: newIndex });
+	}
+
+	render() {
+		const { activeIndex } = this.state;
+
+		const slides = items.map(item => {
+			return (
+				<CarouselItem onExiting={this.onExiting} onExited={this.onExited} key={item.src}>
+					<img src={item.src} alt={item.altText} />
+				</CarouselItem>
+			);
+		});
+
+		return (
+			<Carousel activeIndex={activeIndex} next={this.next} previous={this.previous}>
+				{slides}
+			</Carousel>
+		);
+	}
+}
+
+export default Caroussel;
--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,13 +1,68 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { CardGroup, Card, CardImg, CardText, CardBody, Button, Col } from 'reactstrap';
+import arenaD from './arenaD.jpg';
+import Caroussel from './Caroussel';
+import MarvelD from './MarvelD.jpg';
 
-const LandingPage = () => {
-    return (
-        <div>
-            <NavLink to='/arena'><button>click here to enter in the arena</button></NavLink>
-            <NavLink to='/selecthero'><button>click here to select your hero</button></NavLink>
-        </div>
-    );
-}
+const LandingPage = props => {
+	return (
+		<div style={{ background: 'black' }}>
+			<div style={{ display: 'flex', justifyContent: 'center', paddingTop: '5vh' }}>
+				<Col xl='5' lg='5' md='5' sm='8'>
+					<CardImg top width='100%' src={MarvelD} alt='Card image cap' />
+				</Col>
+			</div>
+			<Col
+				xl={{ size: '10', offset: 1 }}
+				lg={{ size: '10', offset: 1 }}
+				md={{ size: '10', offset: 1 }}
+				style={{ marginTop: '5vh' }}
+			>
+				<Caroussel />
+			</Col>
+			<CardGroup style={{ marginTop: '5vh' }}>
+				<Col xl={{ size: '5', offset: 1 }} lg={{ size: '5', offset: 1 }} md={{ size: '10', offset: 1 }}>
+					<Card style={{ border: '1px solid black' }}>
+						<CardImg top width='100%' src={arenaD} alt='Card image cap' />
+
+						<CardBody style={{ background: 'black', color: 'white' }}>
+							<CardText style={{ textAlign: 'justify' }}>
+								Are you ready to fight ? Enter the arena with a random hero and fight your opponent and witness EPIC
+								battles throughout a large superhero universe and more!
+							</CardText>
+							<div style={{ display: 'flex', justifyContent: 'center' }}>
+								<NavLink to='/arena'>
+									<Button style={{ backgroundColor: '#ED1D24', border: '1px solid black' }}>Enter the arena</Button>
+								</NavLink>
+							</div>
+						</CardBody>
+					</Card>
+				</Col>
+				<Col xl={{ size: '5', offset: 0 }} lg={{ size: '5', offset: 0 }} md={{ size: '10', offset: 1 }}>
+					<Card style={{ border: '1px solid black' }}>
+						<CardImg
+							top
+							width='100%'
+							src='http://www.wow247.co.uk/wp-content/uploads/2014/09/Avengers-main.jpg'
+							alt='Card image cap'
+						/>
+						<CardBody style={{ background: 'black', color: 'white' }}>
+							<CardText style={{ textAlign: 'justify' }}>
+								You are more of a carreful type of guy and don't trust your luck ? Search any hero in our card
+								collection search bar and control your destiny! Will you beat them all?
+							</CardText>
+							<div style={{ display: 'flex', justifyContent: 'center' }}>
+								<NavLink to='/selecthero'>
+									<Button style={{ backgroundColor: '#162CA2', border: '1px solid black' }}>Select your hero</Button>
+								</NavLink>
+							</div>
+						</CardBody>
+					</Card>
+				</Col>
+			</CardGroup>
+		</div>
+	);
+};
 
 export default LandingPage;
