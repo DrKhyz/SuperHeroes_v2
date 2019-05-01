6586f23f7db76f9f3caceca7a5d51940b36bfafe
Baptiste Hyver <baptiste.hyver@gmail.com>
2019-05-01 12:12:45 +0200

    stop the fight when clicking on the randomHeroButton and make disapear the button fight while the fight is occuring so we cannot click on it anymore to avoid weird situations

--- src/SelectHero.jsx
+++ src/SelectHero.jsx
@@ -10,7 +10,7 @@ class SelectHero extends Component {
 		this.state = {
 			search: '',
 			heroStore: [],
-			loadingHeroStore: true,
+			loadingHeroStore: true
 		};
 		this.myInterval = null;
 	}
@@ -69,23 +69,23 @@ class SelectHero extends Component {
 							combat: parseInt(data.powerstats.combat),
 							life: Math.floor(
 								(parseInt(data.powerstats.durability) + parseInt(data.powerstats.intelligence)) *
-									(data.powerstats.speed / 10),
+									(data.powerstats.speed / 10)
 							),
-							barColor: 'success',
+							barColor: 'success'
 						},
 						biography: {
 							'full-name': data.biography['full-name'],
 							publisher: data.biography.publisher,
-							alignment: data.biography.alignment,
+							alignment: data.biography.alignment
 						},
 						appearance: {
 							gender: data.appearance.gender,
 							race: data.appearance.race,
 							height: data.appearance.height[1],
-							weight: data.appearance.weight[1],
+							weight: data.appearance.weight[1]
 						},
-						image: data.image,
-					},
+						image: data.image
+					}
 				});
 			});
 	};
@@ -118,23 +118,23 @@ class SelectHero extends Component {
 					life: Math.floor(
 						(parseInt(this.state.heroStore[i].powerstats.durability) +
 							parseInt(this.state.heroStore[i].powerstats.intelligence)) *
-							(this.state.heroStore[i].powerstats.speed / 10),
+							(this.state.heroStore[i].powerstats.speed / 10)
 					),
-					barColor: 'success',
+					barColor: 'success'
 				},
 				biography: {
 					'full-name': this.state.heroStore[i].biography['full-name'],
 					publisher: this.state.heroStore[i].biography.publisher,
-					alignment: this.state.heroStore[i].biography.alignment,
+					alignment: this.state.heroStore[i].biography.alignment
 				},
 				appearance: {
 					gender: this.state.heroStore[i].appearance.gender,
 					race: this.state.heroStore[i].appearance.race,
 					height: this.state.heroStore[i].appearance.height[1],
-					weight: this.state.heroStore[i].appearance.weight[1],
+					weight: this.state.heroStore[i].appearance.weight[1]
 				},
-				image: this.state.heroStore[i].image,
-			},
+				image: this.state.heroStore[i].image
+			}
 		});
 		this.getCaracter2();
 	};
